71a7dd164ff7bf15b35d6878cd1494221b4f96f4
tanmoyyysarkar <tanmoysarkar.ts1234@gmail.com>
2026-09-21 21:29:14 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import express from "express"
+import cors from "cors";
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.json({
+    status: "ok",
+    message: "FitCoach API is running",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
